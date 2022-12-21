be77eb55984e56f7c5795e2568f9e58b1e10f3a4
sagar <sagar@Sagar-PC>
2022-12-21 09:36:40 +0530

    changed method call.

---
+++ src/test/java/scripts/InvalidLogin.java
@@ -0,0 +1,37 @@
+package scripts;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.Util;
+import page.EnterTimeTrackPage;
+import page.LoginPage;
+
+public class InvalidLogin extends BaseTest {
+
+	@Test
+	public void testInvalidLogin()
+	{
+		//read from Excel file
+		String un = Util.getXlData(XL_PATH, "LoginData", 2, 0);
+		String pw = Util.getXlData(XL_PATH, "LoginData", 2, 1);
+		
+		//1.enter invalid username
+		LoginPage login=new LoginPage(driver, test);
+		login.setUserName(un);
+		
+		
+		//2.enter invalid password
+		login.setPassword(pw);
+		
+		//3.click login button
+		login.clickLoginButton();
+		
+		//4.verify error msg is displayed
+		EnterTimeTrackPage ettPage=new EnterTimeTrackPage(driver, test);
+		ettPage.verifyHomePageIsDisplayed(wait);
+		
+		
+	}
+}
