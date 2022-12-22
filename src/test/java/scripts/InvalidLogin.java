4cd87fd3c8b996990f07dc7edc9bafba37dd6210
sagar <sagar@Sagar-PC>
2022-12-22 07:51:43 +0530

    created new TC2.

--- src/test/java/scripts/InvalidLogin.java
+++ src/test/java/scripts/InvalidLogin.java
@@ -1,40 +1,32 @@
 package scripts;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
 import generic.Util;
-import page.EnterTimeTrackPage;
 import page.LoginPage;
 
-public class InvalidLogin extends BaseTest {
-
+public class InvalidLogin extends BaseTest{
+	
 	@Test(priority = 2)
-	public void testInvalidLogin()
-	{
-		//read from Excel file
-		String un = Util.getXlData(XL_PATH, "InvalidLogin", 1, 0);
-		String pw = Util.getXlData(XL_PATH, "InvalidLogin", 1, 1);
+	public void testInvalidLogin() {
+		String un=Util.getXlData(XL_PATH,"InvalidLogin",1,0);
+		String pw=Util.getXlData(XL_PATH,"InvalidLogin",1,1);
 		
-		//1.enter invalid username
-		LoginPage login=new LoginPage(driver, test);
-		login.setUserName(un);
+//		1. enter invalid user name
+		LoginPage loginPage=new LoginPage(driver, test);
+		loginPage.setUserName(un);
 		
+//		2. enter invalid password
+		loginPage.setPassword(pw);
 		
-		//2.enter invalid password
-		login.setPassword(pw);
+//		3. click login
+		loginPage.clickLoginButton();
 		
-		//3.click login button
-		login.clickLoginButton();
-		
-		//4.verify error msg is displayed
-		EnterTimeTrackPage ettPage=new EnterTimeTrackPage(driver, test);
-		boolean result = ettPage.verifyHomePageIsDisplayed(wait);
-		Assert.assertTrue(result, "Home page is not displayed");
-		
-		
-		
+//		4. err msg should be displayed
+		boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
+		Assert.assertTrue(result,"Err Msg is not displayed");
 	}
+
 }
